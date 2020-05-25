36061808f9f0e4703318a4a535fa95ec56604842
vivket <vivket@repup.co>
2020-05-25 20:47:15 +0530

    added pre processing of data.json and parsed it to escape unicode

--- search_server/search_utility/pre_process_data.py
+++ search_server/search_utility/pre_process_data.py
@@ -1,4 +1,49 @@
+from unidecode import unidecode
+from database import books_keyword_map, data
+
+
 class DataPreProcessor:
     def __init__(self, data):
         self.data = data
         self.books_keyword_map = {}
+
+    def parse_data(self):
+        """
+        modifying the data in place after escaping and decoding
+        :return:
+        """
+        summaries = self.data.get("summaries", [])
+
+        for summary in summaries:
+            summary["summary"] = unidecode(summary.get("summary").decode('unicode-escape'))
+
+    def process_data(self):
+        summaries = self.data.get("summaries", [])
+        titles = self.data.get("titles", [])
+
+        # creating a dictionary of words of summaries as key and value as list of books containing this word/key
+        for summary in summaries:
+            book_id = summary.get("id")
+            book_title = unidecode(titles[book_id].decode('unicode-escape'))
+
+            # combining book_title with the summary so that book name can also be included in search
+            book_summary = "{} {}".format(book_title, summary.get("summary"))
+            keys = book_summary.replace("The Book in Three Sentences: ", "").replace(".", "").split()
+
+            for key in keys:
+                self.books_keyword_map.setdefault(key, {})
+                self.books_keyword_map[key].setdefault(book_id, 0)
+                self.books_keyword_map[key][book_id] += 1
+
+        for key, value in self.books_keyword_map.items():
+            self.books_keyword_map[key] = sorted(value.items(), key=lambda item: item[1])
+
+    def start(self):
+        self.parse_data()
+        self.process_data()
+        return self.books_keyword_map
+
+
+
+pre_process = DataPreProcessor(data)
+books_keyword_map.update(pre_process.start())
